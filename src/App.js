2ec07c3b97173f3b44eb77bc13428b17347b62d8
Felix HUANG <felix.hng7@gmail.com>
2020-10-26 14:20:06 +0200

    parsing datas into hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 
 async function datas() {
@@ -13,10 +15,39 @@ async function datas() {
 }
 
 function App() {
-  const logs = datas();
-  logs.then(function (log) {
-    console.log(log)
-  });
+  const [loadClass, setloadClass] = useState([]);
+  const [searchClasspath, setSearchClasspath] = useState([]);
+  const [searchFiles, setSearchFiles] = useState([]);
+  const [replaceExtendedMetaClasses, setReplaceExtendedMetaClasses] = useState([]);
+  const [ViewRepositoryClientImpl, setViewRepositoryClientImpl] = useState([]);
+  const [ViewRepositoryImpl, setViewRepositoryImpl] = useState([]);
+  const [WebJARs, setWebJARs] = useState([]);
+
+
+  async function dataParse() {
+    const logs = datas();
+    logs.then(function (log) {
+      for (let i = 0; i < log.length; i++) {
+        if (log[i] != undefined) {
+          if (log[i]["loadClass"] != undefined) setloadClass(loadClass => loadClass.concat(log[i]["loadClass"]))
+          if (log[i]["Messages.searchClasspath"] != undefined) setSearchClasspath(loadClass => loadClass.concat(log[i]["Messages.searchClasspath"]))
+          if (log[i]["Messages.searchFiles"] != undefined) setSearchFiles(loadClass => loadClass.concat(log[i]["Messages.searchFiles"]))
+          if (log[i]["Metadata.replaceExtendedMetaClasses"] != undefined) setReplaceExtendedMetaClasses(loadClass => loadClass.concat(log[i]["Metadata.replaceExtendedMetaClasses"]))
+          if (log[i]["ViewRepository.init.ViewRepositoryClientImpl"] != undefined) setViewRepositoryClientImpl(loadClass => loadClass.concat(log[i]["ViewRepositoryClientImpl"]))
+          if (log[i]["ViewRepository.init.ViewRepositoryImpl"] != undefined) setViewRepositoryImpl(loadClass => loadClass.concat(log[i]["ViewRepositoryImpl"]))
+          if (log[i]["WebJARs"] != undefined) setWebJARs(loadClass => loadClass.concat(log[i]["WebJARs"]))
+        }
+      }
+    });
+
+  }
+
+  useEffect(async () => {
+    await dataParse()
+  }, [])
+
+
+  console.log(searchClasspath)
   return (
     <div className="App">
       <header className="App-header">
